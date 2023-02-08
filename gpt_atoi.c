0ffca1341fff1a5fac84ac815406cc807812c9f7
Yekanat <athekkan@student.42roma.it>
2023-02-08 15:14:25 +0100

    added gpt_atoi

---
+++ gpt_atoi.c
@@ -0,0 +1,30 @@
+#include <limits.h>
+#include <stdlib.h>
+
+int	ft_atoi(const char *str)
+{
+	int		i;
+	int		sign;
+	long	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
+		|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		if (result > LONG_MAX || result < LONG_MIN)
+			return ((sign == 1) ? -1 : 0);
+		i++;
+	}
+	return (result * sign);
+}
