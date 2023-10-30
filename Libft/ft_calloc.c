f51a6c56fcca72f0150df5578e498973a3e32593
Angelo Thekkanath <athekkan@e4r3p4.42roma.it>
2023-10-30 22:31:46 +0100

    last check and update

--- Libft/ft_calloc.c
+++ Libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: athekkan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 16:18:54 by athekkan          #+#    #+#             */
-/*   Updated: 2023/02/14 15:10:05 by athekkan         ###   ########.fr       */
+/*   Updated: 2023/02/17 17:04:15 by athekkan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,15 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
+	size_t	bytes;
 	void	*ptr;
-	size_t	i;
 
-	i = count * size;
-	ptr = malloc(i);
-	if (i / size != count)
-		return (0);
-	if (!ptr)
+	bytes = count * size;
+	if (size && (bytes / size) != count)
 		return (NULL);
-	while (i--)
-		((char *)ptr)[i] = 0;
+	ptr = malloc(bytes);
+	if (NULL == ptr)
+		return (NULL);
+	ft_bzero(ptr, bytes);
 	return (ptr);
 }
